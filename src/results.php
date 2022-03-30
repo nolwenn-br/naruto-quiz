1621a636e30e7b4167e11e35f53e0adce4325bbe
nolwenn-br <lmaddec@gmail.com>
2022-03-30 16:29:27 +0200

    page results done

---
+++ src/results.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8" lang="FR">
+	
+	<title>Naruto Quiz - Vos résultats</title>
+
+    <!-- BOOTSTRAP CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- CUSTOM CSS -->
+	<link rel="stylesheet" type="text/css" href="public/style.css" />
+</head>
+
+<body class="px-5 mx-2">
+	<div class="container-fluid">
+		<h1 class="text-center m-1 p-3">Résultats du quiz</h1>
+        <h4 class="text-center">sur 5</h4>
+	</div>
+
+
+</body>
+
+</html>
